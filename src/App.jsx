326990ffb05404c58d24502233b205465986b642
Aatish Kore <aatishkore99@gmail.com>
2025-11-09 07:35:46 +0530

    touch up

--- src/App.jsx
+++ src/App.jsx
@@ -159,7 +159,7 @@ const App = () => {
             <div className="btmbar w-full px-10 py-12 bg-linear-to-t from-black to-transparent absolute bottom-0 text-white">
               <div className="flex items-center gap-4 ">
                 <i className="ri-arrow-down-line text-4xl"></i>
-                <h3 className="font-[sans-serif] text-xl">Scroll Down</h3>
+                <h3 className="font-[Russo_One] text-xl">Scroll Down</h3>
               </div>
               <img
                 src="/ps5_xbox.png"
@@ -177,18 +177,22 @@ const App = () => {
                   className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-600 hover:scale-105 hover:shadow-[0_0_25px_rgba(255,255,255,0.5)] rounded-2xl"
                 />
               </div>
-              <div className="right w-[30%] py-10">
+              <div className="right w-[30%]">
                 <h1 className="text-8xl">Still running,</h1>
                 <h1 className="text-7xl">Not Hunting</h1>
-                <p className="text-xl font-[Helvetica_Now_Display] mt-10">
+                <p className="text-xl font-[Russo_One] mt-10">
                   Dive into the sun-soaked streets of Vice City and the gritty
                   underworld of Leonida. Two rebellious souls. One high-stakes
                   score gone wrong. Every moment counts.
                 </p>
-                <p className="text-xl font-[Helvetica_Now_Display] mt-4">
+                <p className="text-xl font-[Russo_One] mt-4">
+                  Power, money, and survival — everything’s on the line in Vice
+                  City.
+                </p>
+                <p className="text-xl font-[Russo_One] mt-4">
                   Gear up. Get ready. The streets are watching.
                 </p>
-                <button className="bg-yellow-500 px-10 py-5 rounded-xl text-black mt-10 text-4xl hover:scale-[1.1] transition duration-450">
+                <button className="bg-yellow-500 px-10 py-5 rounded-xl text-black mt-5 text-4xl hover:scale-[1.1] transition duration-450">
                   <a
                     href="https://www.rockstargames.com/VI"
                     target="_blank"
